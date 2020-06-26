e1c7a2ae34032c94ab95ee1b262a0f43c121d841
Jeet Dholakia <onemediasoft@gmail.com>
2020-06-26 22:34:26 +0530

    Update PostApi.kt

--- app/src/main/java/com/knitterassignment/network/PostApi.kt
+++ app/src/main/java/com/knitterassignment/network/PostApi.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 
 interface PostApi {
 
+    // TODO: 6/26/20 Add the token via http interceptor later on
     @GET("posts?format=json&access-token=dv8RtNyd5a6ubIJhHlPCNkIVyedc8hx4RnGD?page=1")
     suspend fun getPosts(): Response
 
